4d6e7de32633da9b2fa2601d5d4496d6a88a9e81
iXLoveMyMom2 <159090658+iXLoveMyMom2@users.noreply.github.com>
2024-03-16 21:27:45 +0300

    pk

--- pk.lua
+++ pk.lua
@@ -1,14 +1,1 @@
-p99 = gg.prompt({"Protection Key:"}, {""}, {"text"})
-if p99 == nil then
-  gg.alert("KEY_ERROR_***")
-  print("KEY_ERROR_***")
-  os.exit()
-else
-if p99[1] == "0000" then
-  gg.toast("\nProtection key is correct.")
-  else
-  gg.alert("KEY_ERROR_***\nPlease restart the script.")
-  print("KEY_ERROR_***\nPlease restart the script.")
-  os.exit()
-end
-end
+asd
